63fda2e90c70f23a4bb8c4946db8aa9f8020d566
JobeMichael <jobumichael@gmail.com>
2023-07-12 21:53:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import * as data from "./data.json" assert { type: "json" };
+
+const app = express();
+const port = 8080; // default port to listen
+
+app.use(express.json());
+let plugins = data;
+
+// Create a router instance for the API namespace
+const apiRouter = express.Router();
+
+// Define routes within the API namespace
+apiRouter.get("/plugins", (req, res) => {
+  res.send(plugins);
+});
+
+apiRouter.post("/plugins", (req, res) => {
+  plugins = null;
+  console.log(plugins);
+  res.send("This is the data endpoint");
+});
+
+// Mount the API router under the '/api' namespace
+app.use("/api", apiRouter);
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
